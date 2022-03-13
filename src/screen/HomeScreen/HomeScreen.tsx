dd6d659a42bad252c5074149591f6433954351e9
Victor Inácio <chustrupgamer@gmail.com>
2022-03-12 23:40:18 -0300

    Screen design made

---
+++ src/screen/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import {
+  Container,
+  Title,
+  NotesList,
+  NoteItem,
+  NoteTitle,
+  NoteItemButtons,
+  Button,
+} from './style';
+
+import MUI from 'react-native-vector-icons/MaterialIcons';
+import FA from 'react-native-vector-icons/FontAwesome';
+
+const data = new Array(50).fill(0);
+
+const HomeScreen: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <Title>MY NOTES</Title>
+
+        <NotesList>
+          {data.map((_, index) => {
+            return (
+              <NoteItem key={index}>
+                <NoteTitle>NOTE TITLE</NoteTitle>
+                <NoteItemButtons>
+                  <Button>
+                    <MUI size={24} color="#FFF" name="open-in-new" />
+                  </Button>
+                  <Button>
+                    <FA size={24} color="#FFF" name="pencil-square-o" />
+                  </Button>
+                </NoteItemButtons>
+              </NoteItem>
+            );
+          })}
+        </NotesList>
+      </Container>
+    </>
+  );
+};
+
+export default HomeScreen;
---
+++ src/screen/HomeScreen/style.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native';
+
+const blueColor = '#4F7CAC';
+const orangeColor = '#FF7F11';
+
+export const Container = styled.SafeAreaView`
+  background-color: ${blueColor};
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-weight: bold;
+  font-size: 36px;
+  color: ${orangeColor};
+  margin-bottom: 40px;
+  margin-top: 10px;
+`;
+
+export const NotesList = styled.ScrollView`
+  width: 100%;
+  flex: 1;
+  padding: 0 20px;
+`;
+
+export const NoteItem = styled.TouchableOpacity`
+  width: 100%;
+  background-color: ${orangeColor};
+  border-radius: 5px;
+  color: white;
+  padding: 10px;
+  margin-bottom: 35px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NoteTitle = styled.Text`
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+export const NoteItemButtons = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Button = styled.TouchableOpacity`
+  margin-right: 10px;
+`;
