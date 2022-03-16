0dc3de86ca4198faafdb93d9225e048d3471966f
Victor Inácio <chustrupgamer@gmail.com>
2022-03-16 15:35:05 -0300

    Create Notes Screen ready

--- src/screen/CreateNoteScreen/CreateNoteScreen.tsx
+++ src/screen/CreateNoteScreen/CreateNoteScreen.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
-import {KeyboardAvoidingView, Platform, ScrollView} from 'react-native';
 
 import DefaultScreenLayout from '../../components/DefaultScreenLayout/DefaultScreenLayout';
+import AsyncStorage from '@react-native-community/async-storage';
+import {Alert, KeyboardAvoidingView, Platform, ScrollView} from 'react-native';
 import {TitleInput, ContentInput, SubmitButton, ButtonText} from './style';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-const CreateNoteScreen: React.FC = () => {
+interface Note {
+  title: string;
+  content: string;
+}
+
+const CreateNoteScreen: React.FC<NativeStackScreenProps<any>> = ({
+  navigation,
+}) => {
+  const [title, setTitle] = React.useState('');
+  const [content, setContent] = React.useState('');
+
   return (
     <DefaultScreenLayout title="CREATE NOTE">
       <ScrollView style={{width: '100%'}}>
@@ -14,21 +26,52 @@ const CreateNoteScreen: React.FC = () => {
             alignItems: 'center',
             display: 'flex',
           }}>
-          <TitleInput placeholder="Title" placeholderTextColor={'white'} />
+          <TitleInput
+            placeholder="Title"
+            placeholderTextColor={'white'}
+            value={title}
+            onChangeText={setTitle}
+          />
           <ContentInput
             placeholder="Content"
             placeholderTextColor={'white'}
             multiline={true}
             numberOfLines={20}
             textAlignVertical={'top'}
+            value={content}
+            onChangeText={setContent}
           />
-          <SubmitButton>
+          <SubmitButton onPress={onSubmit}>
             <ButtonText>SAVE</ButtonText>
           </SubmitButton>
         </KeyboardAvoidingView>
       </ScrollView>
     </DefaultScreenLayout>
   );
+
+  async function onSubmit() {
+    if (!title || !content) {
+      Alert.alert('Please fill in all fields before proceeding');
+      return;
+    }
+
+    try {
+      const allNotesJSONString: string | null = await AsyncStorage.getItem(
+        'notes',
+      );
+      const allNotesParsed = allNotesJSONString
+        ? JSON.parse(allNotesJSONString)
+        : {};
+
+      allNotesParsed[Date.now()] = {title, content};
+
+      await AsyncStorage.setItem('notes', JSON.stringify(allNotesParsed));
+
+      navigation.navigate('Home');
+    } catch (e) {
+      Alert.alert('Error saving data.');
+    }
+  }
 };
 
 export default CreateNoteScreen;
